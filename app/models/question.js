73222fa90e7acb3e77831a0af44c264c46a96d6f
Tony Fabery <tfabery@gmail.com>
2016-08-19 16:58:45 -0700

    MVP

--- app/models/question.js
+++
@@ -1,9 +0,0 @@
-import DS from 'ember-data';
-
-export default DS.Model.extend({
-  content: DS.attr(),
-  author: DS.attr(),
-  notes: DS.attr(),
-  answers: DS.hasMany('answer', {async: true}),
-  timestamp: DS.attr()
-});
